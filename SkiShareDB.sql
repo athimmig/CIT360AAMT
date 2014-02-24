bc99c49906ecd22c7a3c1ae551246ba345152391
athimmig <athimmig@gmail.com>
2014-02-24 12:07:36 -0700

    New Stuff Everyone

---
+++ SkiShareDB.sql
@@ -0,0 +1,17 @@
+USE skishareDB
+
+DROP TABLE IF EXISTS skiResorts;
+
+CREATE TABLE IF NOT EXISTS skiResorts
+	( resort_id				INT UNSIGNED		PRIMARY KEY AUTO_INCREMENT
+	, resort_name			VARCHAR(50)				NOT NULL
+	, lattitude 			DECIMAL(9,6)			NOT NULL
+	, logitude				DECIMAL(9,6)			NOT NULL) ENGINE=InnoDB;
+
+-- Load the data from a file, don't forget the \n after the \r on Windows or it won't work.
+LOAD DATA INFILE 'C:/Data/mysql/SkiResortsCSV.csv'
+INTO TABLE skiResorts
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n';
