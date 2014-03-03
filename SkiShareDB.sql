1c72d48826f9ca63755b4f50d0bf349ff497ccfc
arnoldmutariswa <arnoldmutariswa@gmail.com>
2014-03-03 12:10:44 -0700

    server
    resort sever

--- SkiShareDB.sql
+++ SkiShareDB.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS skiResorts
 	, logitude				DECIMAL(9,6)			NOT NULL) ENGINE=InnoDB;
 
 -- Load the data from a file, don't forget the \n after the \r on Windows or it won't work.
-LOAD DATA INFILE 'C:/Data/mysql/SkiResortsCSV.csv'
+LOAD DATA LOCAL INFILE '/Users/arnoldmutariswa/Desktop/class/CIT360AAMT/SkiResortsCSV.csv'
 INTO TABLE skiResorts
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
